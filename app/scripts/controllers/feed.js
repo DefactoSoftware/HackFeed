9ec634d40686a0b7f953f0d2133b1984ffb79049
Romke Bon <romke@bonsens.nl>
2014-06-28 21:54:35 +0200

    Add nested comments stub

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -1,27 +1,48 @@
 'use strict';
 
 angular.module('hackfeedApp')
-  .controller('FeedCtrl', ['$scope', '$firebase',
-    function ($scope, $firebase) {
+
+  .controller('FeedCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
 
     var commentsRef = new Firebase("https://wellfed.firebaseio.com/comments");
     $scope.posts = $firebase(commentsRef);
 
-      $scope.addComment = function () {
-        if ($scope.body) {
-          var keys = $scope.comments.$getIndex();
+    $scope.addPost = function() {
+      if ($scope.body) {
+        var keys = $scope.posts.$getIndex();
 
-          $scope.comments.$add({
-            "body": $scope.body,
-            "uid": $scope.currentUser.id,
-            "authorName": $scope.currentUser.displayName,
-            "createdAt": new Date(),
+        $scope.posts.$add({
+          "body": $scope.body,
+          "uid": $scope.currentUser.id,
+          "authorName": $scope.currentUser.displayName,
+          "createdAt": new Date(),
             $priority: -keys.length
+        });
+        $scope.body = "";
+      }
+    };
 
-          });
-          $scope.body = "";
-        }
-
-      };
+    $scope.enterComment = function(e) {
+      if (e.keyCode != 13) return;
+      //TODO: addComment();
     }
+
+    $scope.addComment = function(postId) {
+      if ($scope['comment-'+postId]) {
+
+        var postRef = new Firebase("https://wellfed.firebaseio.com/comments/"+postId+"/comments");
+        var comments = $firebase(postRef);
+
+        comments.$add({
+          "body": $scope.comment,
+          "uid": $scope.currentUser.id,
+          "authorName": $scope.currentUser.displayName,
+          "createdAt": new Date()
+        });
+        $scope['comment-'+postId] = "";
+      }
+    };
+
+
+   }
   ]);
