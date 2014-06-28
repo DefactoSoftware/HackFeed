be4e72dcebe3cbb692c260c55372f61654f715aa
Romke Bon <romke@bonsens.nl>
2014-06-28 17:55:10 +0200

    Read comment feed from Firebase
    ...maybe

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('hackfeedApp')
-  .controller('FeedCtrl', ['$scope', function ($scope) {
+  .controller('FeedCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
     $scope.mockObject = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var commentsRef = new Firebase("https://wellfed.firebaseio.com/comments");
+    $scope.people = $firebase(commentsRef);
+
   }]);
